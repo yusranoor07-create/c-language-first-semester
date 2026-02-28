a1fd16135aad198195d184dcd4a02e88d651b863
Syed <syeddaniyalali.ct025neduet@gmail.com>
2026-03-01 00:29:24 +0500

    Squashed 'Fibonacci-Series-using-while-loop/' content from commit 8db8319
    git-subtree-dir: Fibonacci-Series-using-while-loop
    git-subtree-split: 8db8319868e7d9954f642ded93399bfab35ebb5f

---
+++ Fibonacci series.c
@@ -0,0 +1,28 @@
+// YUSRA NOOR CT 25075
+#include<stdio.h>
+int main() {
+    int a = 1, b = 1, next = 0, sum = 0;
+
+    printf("Fibonacci series up to 1000 (using while loop):\n");
+    printf("%d %d ", a, b);
+
+    while (1) {
+        next = a + b;
+        if (next > 1000)
+            break;
+
+        printf("%d ", next);
+
+        if (next % 3 == 0 || next % 5 == 0 || next % 7 == 0) {
+            sum += next;
+        }
+
+        a = b;
+        b = next;
+    }
+
+    printf("\nSum of Fibonacci numbers divisible by 3, 5, or 7 = %d\n", sum);
+
+    return 0;
+}
+
